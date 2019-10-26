8e443995dfc333cc87123e49b210c9a2cba2e953
cmlacrd <camilacardoso96@hotmail.com>
2019-10-26 18:52:44 -0200

    creating web server twith express

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const http = require('http');
-
-const hostname = 'localhost';
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.listen(port,'0.0.0.0', function(){
+    console.log('server running on port: '+port);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname +'/tmpl'+'/index.html');
 });
-
